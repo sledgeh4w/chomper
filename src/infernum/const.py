445e6bd731a0f555d64c24546c40451a24f476b7
Sh4w <36655200+Sh4ww@users.noreply.github.com>
2022-09-09 21:08:05 +0800

    Raise exception with symbol info when missing symbol is required (#7)

--- src/infernum/const.py
+++ src/infernum/const.py
@@ -1,10 +1,14 @@
-# The address of stack
-STACK_ADDRESS = 0x50000
-STACK_SIZE = 0x50000
-
 # The address of thread local storage (TLS)
 TLS_ADDRESS = 0x10000
 TLS_SIZE = 0x400
+
+# The address of trap
+TARP_ADDRESS = 0x20000
+TRAP_SIZE = 0x10000
+
+# The address of stack
+STACK_ADDRESS = 0x50000
+STACK_SIZE = 0x50000
 
 # The address of module
 MODULE_ADDRESS = 0x1000000
