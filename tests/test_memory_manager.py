db966ebdb11d95d1dd27de9d22f69802101cb072
Sh4w <sledgeh4w@gmail.com>
2023-07-20 14:46:26 +0800

    Refactor implementation

--- tests/test_memory_manager.py
+++
@@ -1,9 +0,0 @@
-def test_alloc(emu_arm64):
-    for n in range(1, 17):
-        buffer = emu_arm64.alloc(2**n)
-        emu_arm64.read_bytes(buffer, 2**n)
-
-
-def test_free(emu_arm64):
-    buffer = emu_arm64.alloc(1024)
-    emu_arm64.free(buffer)
