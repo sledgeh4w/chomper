0c1da15f56d03514ecb1d23b336da58ac7c3aefc
Sh4w <36655200+sledgeh4w@users.noreply.github.com>
2024-05-04 02:08:51 +0800

    Add tests for Objective-C (#74)

--- tests/test_libc.py
+++ tests/test_libc.py
@@ -2,211 +2,223 @@ import pytest
 
 from chomper.exceptions import SymbolMissingException
 
+emu_names = ["emu_arm", "emu_arm64", "emu_ios"]
 
-class TestCStandardLibrary:
-    emu_names = ["emu_arm", "emu_arm64", "emu_ios"]
 
-    @staticmethod
-    def call_symbol(emu, emu_name, symbol_name, *args):
-        if emu_name == "emu_ios":
-            symbol_name = f"_{symbol_name}"
+def call_symbol(emu, emu_name, symbol_name, *args):
+    if emu_name == "emu_ios":
+        symbol_name = f"_{symbol_name}"
 
-            try:
-                emu.find_symbol(symbol_name)
-            except SymbolMissingException:
-                symbol_name = f"__platform{symbol_name}"
+        try:
+            emu.find_symbol(symbol_name)
+        except SymbolMissingException:
+            symbol_name = f"__platform{symbol_name}"
 
-        return emu.call_symbol(symbol_name, *args)
+    return emu.call_symbol(symbol_name, *args)
 
-    @pytest.mark.usefixtures("libc_arm", "libc_arm64")
-    @pytest.mark.parametrize("emu_name", emu_names)
-    def test_malloc(self, request, emu_name):
-        emu = request.getfixturevalue(emu_name)
 
-        result = self.call_symbol(emu, emu_name, "malloc", 16)
+@pytest.mark.usefixtures("libc_arm", "libc_arm64")
+@pytest.mark.parametrize("emu_name", emu_names)
+def test_malloc(request, emu_name):
+    emu = request.getfixturevalue(emu_name)
 
-        assert result != 0
+    result = call_symbol(emu, emu_name, "malloc", 16)
 
-    @pytest.mark.usefixtures("libc_arm", "libc_arm64")
-    @pytest.mark.parametrize("emu_name", emu_names)
-    def test_free(self, request, emu_name):
-        emu = request.getfixturevalue(emu_name)
+    assert result != 0
 
-        addr = self.call_symbol(emu, emu_name, "malloc", 16)
-        self.call_symbol(emu, emu_name, "free", addr)
 
-    @pytest.mark.usefixtures("libc_arm", "libc_arm64")
-    @pytest.mark.parametrize("emu_name", emu_names)
-    def test_memcpy(self, request, emu_name, str_test):
-        emu = request.getfixturevalue(emu_name)
+@pytest.mark.usefixtures("libc_arm", "libc_arm64")
+@pytest.mark.parametrize("emu_name", emu_names)
+def test_free(request, emu_name):
+    emu = request.getfixturevalue(emu_name)
 
-        v1 = emu.create_buffer(16)
-        v2 = emu.create_string(str_test)
+    addr = call_symbol(emu, emu_name, "malloc", 16)
+    call_symbol(emu, emu_name, "free", addr)
 
-        self.call_symbol(emu, emu_name, "memcpy", v1, v2, len(str_test) + 1)
-        result = emu.read_string(v1)
 
-        assert result == str_test
+@pytest.mark.usefixtures("libc_arm", "libc_arm64")
+@pytest.mark.parametrize("emu_name", emu_names)
+def test_memcpy(request, emu_name, input_str):
+    emu = request.getfixturevalue(emu_name)
 
-    @pytest.mark.usefixtures("libc_arm", "libc_arm64")
-    @pytest.mark.parametrize("emu_name", emu_names)
-    def test_memcmp(self, request, emu_name, str_test):
-        emu = request.getfixturevalue(emu_name)
+    v1 = emu.create_buffer(16)
+    v2 = emu.create_string(input_str)
 
-        v1 = emu.create_string(str_test)
-        v2 = emu.create_string(str_test)
-        v3 = emu.create_string(str_test[::-1])
+    call_symbol(emu, emu_name, "memcpy", v1, v2, len(input_str) + 1)
+    result = emu.read_string(v1)
 
-        result = self.call_symbol(emu, emu_name, "memcmp", v1, v2, len(str_test))
+    assert result == input_str
 
-        assert result == 0
 
-        result = self.call_symbol(emu, emu_name, "memcmp", v1, v3, len(str_test))
+@pytest.mark.usefixtures("libc_arm", "libc_arm64")
+@pytest.mark.parametrize("emu_name", emu_names)
+def test_memcmp(request, emu_name, input_str):
+    emu = request.getfixturevalue(emu_name)
 
-        assert result != 0
+    v1 = emu.create_string(input_str)
+    v2 = emu.create_string(input_str)
+    v3 = emu.create_string(input_str[::-1])
 
-    @pytest.mark.usefixtures("libc_arm", "libc_arm64")
-    @pytest.mark.parametrize("emu_name", emu_names)
-    def test_memset(self, request, emu_name, str_test):
-        emu = request.getfixturevalue(emu_name)
+    result = call_symbol(emu, emu_name, "memcmp", v1, v2, len(input_str))
 
-        size = len(str_test)
+    assert result == 0
 
-        v1 = emu.create_string(str_test)
+    result = call_symbol(emu, emu_name, "memcmp", v1, v3, len(input_str))
 
-        self.call_symbol(emu, emu_name, "memset", v1, 0, size)
-        result = emu.read_bytes(v1, size)
+    assert result != 0
 
-        assert result == b"\x00" * size
 
-    @pytest.mark.usefixtures("libc_arm", "libc_arm64")
-    @pytest.mark.parametrize("emu_name", emu_names)
-    def test_strncpy(self, request, emu_name, str_test):
-        emu = request.getfixturevalue(emu_name)
+@pytest.mark.usefixtures("libc_arm", "libc_arm64")
+@pytest.mark.parametrize("emu_name", emu_names)
+def test_memset(request, emu_name, input_str):
+    emu = request.getfixturevalue(emu_name)
 
-        v1 = emu.create_buffer(16)
-        v2 = emu.create_string(str_test)
-        v3 = 5
+    size = len(input_str)
 
-        self.call_symbol(emu, emu_name, "strncpy", v1, v2, v3)
-        result = emu.read_string(v1)
+    v1 = emu.create_string(input_str)
 
-        assert result == str_test[:v3]
+    call_symbol(emu, emu_name, "memset", v1, 0, size)
+    result = emu.read_bytes(v1, size)
 
-    @pytest.mark.usefixtures("libc_arm", "libc_arm64")
-    @pytest.mark.parametrize("emu_name", emu_names)
-    def test_strncmp(self, request, emu_name, str_test):
-        emu = request.getfixturevalue(emu_name)
+    assert result == b"\x00" * size
 
-        v1 = emu.create_string(str_test)
-        v2 = emu.create_string(str_test[:-1] + " ")
 
-        result = self.call_symbol(emu, emu_name, "strncmp", v1, v2, len(str_test) - 1)
+@pytest.mark.usefixtures("libc_arm", "libc_arm64")
+@pytest.mark.parametrize("emu_name", emu_names)
+def test_strncpy(request, emu_name, input_str):
+    emu = request.getfixturevalue(emu_name)
 
-        assert result == 0
+    v1 = emu.create_buffer(16)
+    v2 = emu.create_string(input_str)
+    v3 = 5
 
-        result = self.call_symbol(emu, emu_name, "strncmp", v1, v2, len(str_test))
+    call_symbol(emu, emu_name, "strncpy", v1, v2, v3)
+    result = emu.read_string(v1)
 
-        assert result != 0
+    assert result == input_str[:v3]
 
-    @pytest.mark.usefixtures("libc_arm", "libc_arm64")
-    @pytest.mark.parametrize("emu_name", emu_names)
-    def test_strncat(self, request, emu_name, str_test):
-        emu = request.getfixturevalue(emu_name)
 
-        v1 = emu.create_buffer(32)
-        v2 = emu.create_string(str_test)
-        v3 = 5
+@pytest.mark.usefixtures("libc_arm", "libc_arm64")
+@pytest.mark.parametrize("emu_name", emu_names)
+def test_strncmp(request, emu_name, input_str):
+    emu = request.getfixturevalue(emu_name)
 
-        emu.write_string(v1, str_test)
+    v1 = emu.create_string(input_str)
+    v2 = emu.create_string(input_str[:-1] + " ")
 
-        self.call_symbol(emu, emu_name, "strncat", v1, v2, v3)
-        result = emu.read_string(v1)
+    result = call_symbol(emu, emu_name, "strncmp", v1, v2, len(input_str) - 1)
 
-        assert result == str_test + str_test[:v3]
+    assert result == 0
 
-    @pytest.mark.usefixtures("libc_arm", "libc_arm64")
-    @pytest.mark.parametrize("emu_name", emu_names)
-    def test_strcpy(self, request, emu_name, str_test):
-        emu = request.getfixturevalue(emu_name)
+    result = call_symbol(emu, emu_name, "strncmp", v1, v2, len(input_str))
 
-        v1 = emu.create_buffer(16)
-        v2 = emu.create_string(str_test)
+    assert result != 0
 
-        self.call_symbol(emu, emu_name, "strcpy", v1, v2)
-        result = emu.read_string(v1)
 
-        assert result == str_test
+@pytest.mark.usefixtures("libc_arm", "libc_arm64")
+@pytest.mark.parametrize("emu_name", emu_names)
+def test_strncat(request, emu_name, input_str):
+    emu = request.getfixturevalue(emu_name)
 
-    @pytest.mark.usefixtures("libc_arm", "libc_arm64")
-    @pytest.mark.parametrize("emu_name", emu_names)
-    def test_strcmp(self, request, emu_name, str_test):
-        emu = request.getfixturevalue(emu_name)
+    v1 = emu.create_buffer(32)
+    v2 = emu.create_string(input_str)
+    v3 = 5
 
-        v1 = emu.create_string(str_test)
-        v2 = emu.create_string(str_test)
-        v3 = emu.create_string(str_test[:-1] + " ")
+    emu.write_string(v1, input_str)
 
-        result = self.call_symbol(emu, emu_name, "strcmp", v1, v2)
+    call_symbol(emu, emu_name, "strncat", v1, v2, v3)
+    result = emu.read_string(v1)
 
-        assert result == 0
+    assert result == input_str + input_str[:v3]
 
-        result = self.call_symbol(emu, emu_name, "strcmp", v1, v3)
 
-        assert result != 0
+@pytest.mark.usefixtures("libc_arm", "libc_arm64")
+@pytest.mark.parametrize("emu_name", emu_names)
+def test_strcpy(request, emu_name, input_str):
+    emu = request.getfixturevalue(emu_name)
 
-    @pytest.mark.usefixtures("libc_arm", "libc_arm64")
-    @pytest.mark.parametrize("emu_name", emu_names)
-    def test_strcat(self, request, emu_name, str_test):
-        emu = request.getfixturevalue(emu_name)
+    v1 = emu.create_buffer(16)
+    v2 = emu.create_string(input_str)
 
-        v1 = emu.create_buffer(32)
-        v2 = emu.create_string(str_test)
+    call_symbol(emu, emu_name, "strcpy", v1, v2)
+    result = emu.read_string(v1)
 
-        emu.write_string(v1, str_test)
+    assert result == input_str
 
-        self.call_symbol(emu, emu_name, "strcat", v1, v2)
-        result = emu.read_string(v1)
 
-        assert result == str_test + str_test
+@pytest.mark.usefixtures("libc_arm", "libc_arm64")
+@pytest.mark.parametrize("emu_name", emu_names)
+def test_strcmp(request, emu_name, input_str):
+    emu = request.getfixturevalue(emu_name)
 
-    @pytest.mark.usefixtures("libc_arm", "libc_arm64")
-    @pytest.mark.parametrize("emu_name", emu_names)
-    def test_strlen(self, request, emu_name, str_test):
-        emu = request.getfixturevalue(emu_name)
+    v1 = emu.create_string(input_str)
+    v2 = emu.create_string(input_str)
+    v3 = emu.create_string(input_str[:-1] + " ")
 
-        v1 = emu.create_string(str_test)
+    result = call_symbol(emu, emu_name, "strcmp", v1, v2)
 
-        result = self.call_symbol(emu, emu_name, "strlen", v1)
+    assert result == 0
 
-        assert result == len(str_test)
+    result = call_symbol(emu, emu_name, "strcmp", v1, v3)
 
-    @pytest.mark.usefixtures("libc_arm", "libc_arm64")
-    @pytest.mark.parametrize("emu_name", ["emu_arm", "emu_arm64"])
-    def test_sprintf(self, request, emu_name, str_test):
-        emu = request.getfixturevalue(emu_name)
+    assert result != 0
 
-        fmt = "%d%s"
 
-        v1 = emu.create_buffer(16)
-        v2 = emu.create_string(fmt)
-        v3 = len(str_test)
-        v4 = emu.create_string(str_test)
+@pytest.mark.usefixtures("libc_arm", "libc_arm64")
+@pytest.mark.parametrize("emu_name", emu_names)
+def test_strcat(request, emu_name, input_str):
+    emu = request.getfixturevalue(emu_name)
 
-        self.call_symbol(emu, emu_name, "sprintf", v1, v2, v3, v4)
-        result = emu.read_string(v1)
+    v1 = emu.create_buffer(32)
+    v2 = emu.create_string(input_str)
 
-        assert result == f"{len(str_test)}{str_test}"
+    emu.write_string(v1, input_str)
 
-    @pytest.mark.usefixtures("libc_arm", "libc_arm64")
-    @pytest.mark.parametrize("emu_name", ["emu_arm", "emu_arm64"])
-    def test_printf(self, request, emu_name, str_test):
-        emu = request.getfixturevalue(emu_name)
+    call_symbol(emu, emu_name, "strcat", v1, v2)
+    result = emu.read_string(v1)
 
-        fmt = "%d%s"
+    assert result == input_str + input_str
 
-        v1 = emu.create_string(fmt)
-        v2 = len(str_test)
-        v3 = emu.create_string(str_test)
 
-        self.call_symbol(emu, emu_name, "printf", v1, v2, v3)
+@pytest.mark.usefixtures("libc_arm", "libc_arm64")
+@pytest.mark.parametrize("emu_name", emu_names)
+def test_strlen(request, emu_name, input_str):
+    emu = request.getfixturevalue(emu_name)
+
+    v1 = emu.create_string(input_str)
+
+    result = call_symbol(emu, emu_name, "strlen", v1)
+
+    assert result == len(input_str)
+
+
+@pytest.mark.usefixtures("libc_arm", "libc_arm64")
+@pytest.mark.parametrize("emu_name", ["emu_arm", "emu_arm64"])
+def test_sprintf(request, emu_name, input_str):
+    emu = request.getfixturevalue(emu_name)
+
+    fmt = "%d%s"
+
+    v1 = emu.create_buffer(16)
+    v2 = emu.create_string(fmt)
+    v3 = len(input_str)
+    v4 = emu.create_string(input_str)
+
+    call_symbol(emu, emu_name, "sprintf", v1, v2, v3, v4)
+    result = emu.read_string(v1)
+
+    assert result == f"{len(input_str)}{input_str}"
+
+
+@pytest.mark.usefixtures("libc_arm", "libc_arm64")
+@pytest.mark.parametrize("emu_name", ["emu_arm", "emu_arm64"])
+def test_printf(request, emu_name, input_str):
+    emu = request.getfixturevalue(emu_name)
+
+    fmt = "%d%s"
+
+    v1 = emu.create_string(fmt)
+    v2 = len(input_str)
+    v3 = emu.create_string(input_str)
+
+    call_symbol(emu, emu_name, "printf", v1, v2, v3)
---
+++ tests/test_objc.py
@@ -0,0 +1,77 @@
+def test_ns_string(emu_ios, objc):
+    string = objc.msg_send("NSString", "stringWithUTF8String:", "chomper")
+
+    assert string
+
+
+def test_ns_array(emu_ios, objc):
+    array = objc.msg_send("NSMutableArray", "array")
+
+    assert array
+
+    string = objc.msg_send("NSString", "stringWithUTF8String:", "chomper")
+    objc.msg_send(array, "addObject:", string)
+
+
+def test_ns_dictionary(emu_ios, objc):
+    dictionary = objc.msg_send("NSMutableDictionary", "dictionary")
+
+    assert dictionary
+
+    string = objc.msg_send("NSString", "stringWithUTF8String:", "chomper")
+    objc.msg_send(dictionary, "setObject:forKey:", string, string)
+
+
+def test_ns_data(emu_ios, objc):
+    data_bytes = b"chomper"
+
+    buffer = emu_ios.create_buffer(len(data_bytes))
+    emu_ios.write_bytes(buffer, data_bytes)
+
+    data = objc.msg_send("NSData", "dataWithBytes:length:", buffer, len(data_bytes))
+
+    assert data
+
+
+def test_ns_url(emu_ios, objc):
+    string = objc.msg_send("NSString", "stringWithUTF8String:", "https://google.com")
+
+    url = objc.msg_send("NSURL", "alloc")
+    objc.msg_send(url, "initWithString:", string)
+
+    assert url
+
+
+def test_ns_request(emu_ios, objc):
+    string = objc.msg_send("NSString", "stringWithUTF8String:", "https://google.com")
+
+    url = objc.msg_send("NSURL", "alloc")
+    objc.msg_send(url, "initWithString:", string)
+
+    request = objc.msg_send("NSMutableURLRequest", "requestWithURL:", url)
+
+    assert request
+
+
+def test_ns_locale(emu_ios, objc):
+    preferred_languages = objc.msg_send("NSLocale", "preferredLanguages")
+
+    assert preferred_languages
+
+    preferred_language = objc.msg_send(preferred_languages, "firstObject")
+    str_ptr = objc.msg_send(preferred_language, "cStringUsingEncoding:", 4)
+
+    assert len(emu_ios.read_string(str_ptr)) > 0
+
+
+def test_ns_user_defaults(emu_ios, objc):
+    user_defaults = objc.msg_send("NSUserDefaults", "standardUserDefaults")
+
+    assert user_defaults
+
+    key = objc.msg_send("NSString", "stringWithUTF8String:", "AppleLocale")
+
+    apple_locale = objc.msg_send(user_defaults, "stringForKey:", key)
+    str_ptr = objc.msg_send(apple_locale, "cStringUsingEncoding:", 4)
+
+    assert len(emu_ios.read_string(str_ptr)) > 0
