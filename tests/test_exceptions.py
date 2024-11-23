11a9e16f0b5471b3c47eb104266c676f9c373451
Sh4w <sledgeh4w@gmail.com>
2024-11-24 00:25:54 +0800

    Retrieve example binary files from the Internet

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -21,10 +21,15 @@ def test_unhandled_system_call_exception():
 
 def test_missing_symbol_required_exception(input_bytes):
     with pytest.raises(EmulatorCrashedException, match=r"Missing symbol.*"):
-        emu = Chomper(arch=ARCH_ARM64)
-        libszstone = emu.load_module(
-            os.path.join(conftest.com_shizhuang_duapp_path, "libszstone.so")
+        binary_path = "examples/binaries/android/com.shizhuang.duapp/libszstone.so"
+        module_path = os.path.join(conftest.base_path, "..", binary_path)
+        conftest.retrieve_binary(
+            url=conftest.BINARY_URL % binary_path,
+            filepath=module_path,
         )
+
+        emu = Chomper(arch=ARCH_ARM64)
+        libszstone = emu.load_module(module_path)
 
         a1 = emu.create_buffer(len(input_bytes))
         a2 = len(input_bytes)
