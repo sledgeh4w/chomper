d4bb84ddfa66a6372e912692c3c092ba92dd660e
Sh4w <sledgeh4w@gmail.com>
2024-12-13 02:02:28 +0800

    Add va_list parameter (#109)

--- src/chomper/objc.py
+++ src/chomper/objc.py
@@ -1,5 +1,5 @@
 from contextlib import contextmanager
-from typing import Union
+from typing import Optional, Sequence, Union
 
 
 class ObjC:
@@ -26,7 +26,13 @@ class ObjC:
         finally:
             self.emu.free(name_ptr)
 
-    def msg_send(self, receiver: Union[int, str], sel: Union[int, str], *args) -> int:
+    def msg_send(
+        self,
+        receiver: Union[int, str],
+        sel: Union[int, str],
+        *args: Union[int, str],
+        va_list: Optional[Sequence[int]] = None
+    ) -> int:
         """Send message to Objective-C runtime.
 
         Args:
@@ -35,6 +41,7 @@ class ObjC:
             sel: The selector. If is str, consider it as the selector name.
             args: Parameters of the method to be called. If a str is passed in,
                 it will be converted to a pointer to a C string.
+            va_list: Variable number of arguments.
         """
         receiver = self.get_class(receiver) if isinstance(receiver, str) else receiver
         sel = self.get_sel(sel) if isinstance(sel, str) else sel
@@ -52,7 +59,9 @@ class ObjC:
                 new_args.append(arg)
 
         try:
-            return self.emu.call_symbol("_objc_msgSend", receiver, sel, *new_args)
+            return self.emu.call_symbol(
+                "_objc_msgSend", receiver, sel, *new_args, va_list=va_list
+            )
         finally:
             for mem_ptr in mem_ptrs:
                 self.emu.free(mem_ptr)
