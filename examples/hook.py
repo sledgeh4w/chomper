60dba4d2dc9bb7ed0a0e21ceb3a3c6da0d459523
Lynnette <3079662103@qq.com>
2025-05-15 11:01:30 +0800

    Add example for memory read and write hook

--- examples/hook.py
+++ examples/hook.py
@@ -3,6 +3,7 @@ import os
 from chomper import Chomper
 from chomper.const import ARCH_ARM64, OS_IOS
 from chomper.objc import ObjC
+from unicorn import UC_MEM_READ
 
 base_path = os.path.abspath(os.path.dirname(__file__))
 
@@ -28,6 +29,12 @@ def hook_ui_device_identifier_for_vendor(uc, address, size, user_data):
     return objc.msg_send("NSUUID", "UUID")
 
 
+def mem_hook(uc, access, address, size, value, user_data):
+    emu = user_data.get("emu")
+    access_str = "READ" if access == UC_MEM_READ else "WRITE"
+    print(f"[{access_str}] addr=0x{address:X}, size={size}, value={value}")
+
+
 def main():
     emu = Chomper(
         arch=ARCH_ARM64,
@@ -45,7 +52,10 @@ def main():
 
     # Hook Objetive-C function by symbol name
     emu.add_hook("+[UIDevice currentDevice]", hook_ui_device_current_device)
-
+    
+    # Hook Memory Read/Write
+    emu.add_mem_hook(mem_hook)
+    
     # Hook and intercept
     emu.add_interceptor("-[UIDevice identifierForVendor]", hook_ui_device_identifier_for_vendor)
 
